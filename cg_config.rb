600515e400dcd2c0c87761cb03ce43a0da5b7575
Pito Salas <pitosalas@gmail.com>
2014-05-29 11:46:49 -0400

    First commit

---
+++ cg_config.rb
@@ -0,0 +1,12 @@
+# Copyright string
+COPYRIGHT_STRING = "Copyright (2013-2014) R. Pito Salas, pitosalas@gmail.com"
+
+# Course short name
+COURSE_SHORT_NAME = "Cosi 000"
+COURSE_LONG_NAME = "Not a course"
+
+# Sections in the right hand margin of the page
+SECTION_CONFIG = [
+    SectionDef.new("Background", "background", type: :section)
+]
+
