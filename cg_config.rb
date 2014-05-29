c3b0c4adad07493dc181293cd4f7412577eaab71
Pito Salas <pitosalas@gmail.com>
2014-05-29 12:09:59 -0400

    update readme

--- cg_config.rb
+++ cg_config.rb
@@ -1,6 +1,9 @@
 # Copyright string
 COPYRIGHT_STRING = "Copyright (2013-2014) R. Pito Salas, pitosalas@gmail.com"
 
+# Bucket Deployment of the course
+AWS_BUCKET = "cosi165-2014"
+
 # Course short name
 COURSE_SHORT_NAME = "Cosi 000"
 COURSE_LONG_NAME = "Not a course"
