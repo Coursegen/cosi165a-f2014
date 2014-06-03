f7f688906f00d8c1991a97932406a873c093b9f7
Pito Salas <pitosalas@gmail.com>
2014-06-03 17:25:36 -0400

    lots of content added

--- cg_config.rb
+++ cg_config.rb
@@ -10,10 +10,18 @@ AWS_BUCKET = "cosi165-2014"
 COURSE_SHORT_NAME = "Cosi 165a"
 COURSE_LONG_NAME = "IT Entrepreneurship"
 
+# Schedule information. Note that Monday is day 0
+LECTURES_SCHEDULE_CONFIG = ScheduleDef.new(
+    first_day: "sep-3-2014", 
+    weekdays: [:monday, :wednesday],
+    number: 26,
+    skips: [])
+
+
 # Sections in the right hand margin of the page
 SECTION_CONFIG = [
-  SectionDef.new("Background", "background", type: :section, collapsed: false),
+  SectionDef.new("Background", "background", type: :section, collapsed: true),
+  SectionDef.new("Lectures", "lectures", type: :lecture, schedule: LECTURES_SCHEDULE_CONFIG),
   SectionDef.new("Incubator", "incubator", type: :section, collapsed: true),
-  SectionDef.new("Lectures", "lectures", type: :lecture),
   SectionDef.new("Topics", "topics", hidden: true, type: :section)
 ]
